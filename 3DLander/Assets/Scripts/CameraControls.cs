e347670d77820e93b824a508cf51e6e2232e14ae
DCHolland <Dcholla@okstate.edu>
2017-03-08 02:31:29 -0600

    Added the camera
    Added camera controller

---
+++ 3DLander/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+	public GameObject Ship;
+	private Vector3 offset;
+	private float horizRotate= 0.0f;
+	private float vertRotate = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		//Gets the vector offset from the intial position of the camera relative to the ship
+		offset = transform.position - Ship.transform.position;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Ship.transform.position + offset;
+		horizRotate =  1.0f * Input.GetAxis ("Mouse X");
+		vertRotate = 1.0f * Input.GetAxis ("Mouse Y");
+		//print (horizRotate);
+
+		//Rotate around the ship, fairly easy stuff
+		transform.RotateAround (Ship.transform.position,transform.up,-horizRotate * 30 *Time.deltaTime);
+		transform.RotateAround (Ship.transform.position, transform.right,vertRotate * 30 * Time.deltaTime);
+
+		//Updates the new offset after rotation
+		offset = transform.position - Ship.transform.position;
+	}
+}
