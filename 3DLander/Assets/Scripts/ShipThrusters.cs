1dc65ce26de7eb3ef76cc38f8cdd0ecb5f4a5822
Joshua Veit <javeit@okstate.edu>
2017-03-09 09:09:19 -0600

    Added Fuel limits and speed check to ship thruster
    I added the fuel limits with public variables to control the speed of
    fuel use and the maximum fuel allowed. Also, I added a way for the ship
    to compare it's velocity to a maximum allowed in order to further define
    a good vs bad landing.

--- 3DLander/Assets/Scripts/ShipThrusters.cs
+++ 3DLander/Assets/Scripts/ShipThrusters.cs
@@ -8,32 +8,53 @@ public class ShipThrusters : MonoBehaviour {
 	public float thrusterPowerIncrement; //controls how quickly the thruster reaches full thrust
 	public float initialForce;           //the initial force, in the positive x direction applied to the ship
 	public float thrusterDelay;          //the number of frames until the thruster kicks in at the start of the game
+	public float maxFuel;
+	public float fuelLeft;
+	public float fuelUseRate;            //rate of fuel usage per second
+	public float vel;
+	public float maxVelocityAllowed;
 
 	private Rigidbody ship;
 	private float thrusterPower = 0;
+	private Rigidbody rigidBody;
 
 	void Start () {
 		ship = GetComponent<Rigidbody> ();  //finds the ship's rigidbody component
 		ship.AddForce (initialForce, 0, 0); //adds initial force defined earlier
+		fuelLeft = maxFuel;
+		rigidBody = GetComponent<Rigidbody> ();
 	}
 
 	void FixedUpdate () {
-		if (thrusterDelay <= 0) {
-			if (Input.GetButton ("Jump")) {
-				thrusterPower += thrusterPowerIncrement; //increments the thruster power as long as the space bar is pressed;
+		if (fuelLeft > 0) {
+			if (thrusterDelay <= 0) {
+				if (Input.GetButton ("Jump")) {
+					thrusterPower += thrusterPowerIncrement; //increments the thruster power as long as the space bar is pressed;
+					fuelLeft -= fuelUseRate * Time.deltaTime;
 
-				if (thrusterPower > maxThrusterPower)
-					thrusterPower = maxThrusterPower;
+					if (thrusterPower > maxThrusterPower)
+						thrusterPower = maxThrusterPower;
 				
-				ship.AddForce (transform.up * thrusterPower);
-			} else {
-				thrusterPower -= thrusterPowerIncrement; //decrements the thruster power as long as the space bar isn't pressed;
+					ship.AddForce (transform.up * thrusterPower);
+				} else {
+					thrusterPower -= thrusterPowerIncrement; //decrements the thruster power as long as the space bar isn't pressed;
 
-				if (thrusterPower < 0)
-					thrusterPower = 0;
+					if (thrusterPower < 0)
+						thrusterPower = 0;
+				}
+			} else {
+				thrusterDelay--;
 			}
-		} else {
-			thrusterDelay--;
 		}
+		vel = rigidBody.velocity.y;
+	}
+
+	void OnCollisionEnter ( Collision col ){
+		if (rigidBody.velocity.y < maxVelocityAllowed) {
+			Debug.Log ("too fast");
+		} else {
+			Debug.Log ("good job");
+		}
+		Destroy (this);
 	}
 }
